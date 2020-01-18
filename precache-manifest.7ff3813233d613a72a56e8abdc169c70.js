ab320ae0a0ace9a0f860fd451c2d649b11a728ec
Yuri V <yuri.valigursky@gmail.com>
2020-01-18 13:53:21 +0200

    deploy

---
+++ precache-manifest.7ff3813233d613a72a56e8abdc169c70.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc21d1e789a18410e40ef4c4e506a74",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "86294585d47cec5de224",
+    "url": "/react-shopping-cart/static/css/2.11cc220a.chunk.css"
+  },
+  {
+    "revision": "1bc9cbbbad78b5cfa266",
+    "url": "/react-shopping-cart/static/css/main.255fd7de.chunk.css"
+  },
+  {
+    "revision": "86294585d47cec5de224",
+    "url": "/react-shopping-cart/static/js/2.fe63c902.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-shopping-cart/static/js/2.fe63c902.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bc9cbbbad78b5cfa266",
+    "url": "/react-shopping-cart/static/js/main.1b9d1881.chunk.js"
+  },
+  {
+    "revision": "c4c31cc5487fef6394fc",
+    "url": "/react-shopping-cart/static/js/runtime-main.2dfb7e38.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/react-shopping-cart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/react-shopping-cart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/react-shopping-cart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/react-shopping-cart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/react-shopping-cart/static/media/item6.f5fd97b5.jpg"
+  }
+]);
